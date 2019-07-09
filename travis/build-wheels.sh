dc7acf19e020ad5c9f5665077ab1aac71cd26096
Tianyi Li <tianyikillua@gmail.com>
2019-07-09 16:54:27 +0200

    Build source wheels before binary ones

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -11,6 +11,7 @@ curl -L https://github.com/swig/swig/archive/rel-4.0.0.tar.gz | tar xz \
 
 # Build wheels for Python 3.6 and 3.7
 cd ~/
+${PYTHON_ROOT_DIR}/bin/python3 setup.py sdist
 for PYVERSION in 6 7; do
     export PYTHON_ROOT_DIR=/opt/python/cp3${PYVERSION}-cp3${PYVERSION}m
     ${PYTHON_ROOT_DIR}/bin/pip install cmake numpy scipy
@@ -18,7 +19,6 @@ for PYVERSION in 6 7; do
     export CMAKE_EXE=${PYTHON_ROOT_DIR}/bin/cmake
     ${PYTHON_ROOT_DIR}/bin/python3 setup.py bdist_wheel --plat-name manylinux2010_x86_64
 done
-${PYTHON_ROOT_DIR}/bin/python3 setup.py sdist
 
 # Copy wheels to /io
 mkdir -p /io/dist/
