18c80f0aa5466076fb58747d08c4569a88561c4c
Tianyi Li <tianyikillua@gmail.com>
2019-11-29 00:05:08 +0100

    Finally works for macOS

--- README.md
+++ README.md
@@ -5,37 +5,13 @@
 
 This repository provides a Python repackaging of the latest version (9.3.0) of the [MEDCoupling](https://docs.salome-platform.org/latest/dev/MEDCoupling/developer/index.html) library via a Python file `setup.py` and hence installable via `pip`.
 
-The initial purpose of this redistribution is to mainly use the remapping algorithms of MEDCoupling (see also [pymapping](https://github.com/tianyikillua/pymapping)). Hence the following MEDCoupling functionalities are NOT supported
+The initial purpose of this redistribution is to mainly use the remapping algorithms of MEDCoupling (see [pymapping](https://github.com/tianyikillua/pymapping) and [paraview-mapping](https://github.com/tianyikillua/paraview-mapping)). Hence the following MEDCoupling functionalities are NOT supported
 
 - MED file reading/writing via `MEDLoader`, so the `MED-file` library is not required (`MEDCOUPLING_MICROMED=ON`)
 - Parallel functionalities (`MEDCOUPLING_ENABLE_PARTITIONER=OFF`)
-- Renumbering (`MEDCOUPLING_ENABLE_RENUMBER=OFF`), depending on `boost`
+- Renumbering (`MEDCOUPLING_ENABLE_RENUMBER=OFF`), which depends on `boost`
 
-### Building from source
-
-Running `python setup.py install` or `pip install .` will
-
-1. Download the source code of MEDCoupling from [salome-platform.org](http://files.salome-platform.org/Salome/other/medCoupling-9.3.0.tar.gz)
-2. Build the C++ / Python libraries using `cmake` and `swig`
-3. Distribute and install generated files using `setuptools`
-
-The building process has been tested under Windows 10 with Visual Studio 2019, under Ubuntu 18.04 with gcc 7.4, and under the [manylinux2010](https://github.com/pypa/python-manylinux-demo) platform.
-
-### Installation from PyPI
-
-To install MEDCoupling, you can also simply use the binary wheels available on PyPI
-```
-pip install -U medcoupling
-```
-
-Binary wheels are available for 64-bit Windows (`win_amd64`) and Linux-like platforms (`manylinux2010_x86_64`) and are built with Python 3.6 and 3.7. Sadly macOS is not supported (consult developers of MEDCoupling).
-
-To assure that MEDCoupling is well installed, try importing it in your Python
-```
-import medcoupling  # should not raise error
-```
-
-### Versioning
+## Versioning
 
 The version of this package (on PyPI) follows that of MEDCoupling. For MEDCoupling 9.3.0 for instance, we use
 ```
@@ -43,7 +19,31 @@ v9.3.0r1, v9.3.0r2, ...
 ```
 to designate all PyPI releases based on MEDCoupling 9.3.0. The suffix `r[x]` is included (abusively, since according to PEP 440 it is for post releases) for possible bug fixes coming from this package.
 
-### Uploading to PyPI
+## Installation from PyPI
+
+To install MEDCoupling, you can now simply use the binary wheels available on PyPI
+```
+pip install -U medcoupling
+```
+
+Binary Python 3.6 and 3.7 wheels are available for 64-bit Windows (`win_amd64`), Linux-like platforms (`manylinux2010_x86_64`) and macOS (`macosx_10_11_x86_64`).
+
+To assure that MEDCoupling is well installed, try importing it in your Python
+```
+import medcoupling  # should not raise error
+```
+
+## Building from source
+
+Running `python setup.py install` or `pip install .` will
+
+1. Download the source code of MEDCoupling from [salome-platform.org](http://files.salome-platform.org/Salome/other/medCoupling-9.3.0.tar.gz)
+2. Build the C++ / Python libraries using `cmake` and `swig`
+3. Distribute and install generated files using `setuptools`
+
+The building process has been tested under Windows 10 with Visual Studio 2019, under Ubuntu 18.04 with gcc 7.4, under the [manylinux2010](https://github.com/pypa/python-manylinux-demo) platform and under macOS 10.15.
+
+## Uploading to PyPI
 
 To upload source and binary wheels to PyPI, type
 ```
@@ -56,14 +56,17 @@ python setup.py bdist_wheel --plat-name win_amd64
 # Binary on Linux
 python3 setup.py bdist_wheel --plat-name manylinux1_x86_64
 
+# Binary on macOS
+python3 setup.py bdist_wheel --plat-name macosx_10_11_x86_64
+
 # Upload to PyPI
 python -m twine upload dist/*
 ```
 
-### License
+## License
 
 MEDCoupling, and hence this repository are published under [LGPL 2.1](https://en.wikipedia.org/wiki/GNU_Lesser_General_Public_License).
 
-### Repository author
+## Repository author
 
 Tianyi Li <tianyikillua@gmail.com>
--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-import codecs
 import glob
 import os
 import platform
@@ -16,7 +15,7 @@ __author__ = "Tianyi Li"
 __email__ = "tianyikillua@gmail.com"
 __copyright__ = "Copyright (c) 2019 {} <{}>".format(__author__, __email__)
 __license__ = "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)"
-__version__ = "9.3.0r3"
+__version__ = "9.3.0r4"
 __status__ = "Development Status :: 4 - Beta"
 
 basedir = os.path.dirname(os.path.realpath(__file__))
@@ -25,7 +24,9 @@ basedir = os.path.dirname(os.path.realpath(__file__))
 def download_build_medcoupling():
 
     # Source file to download
-    MEDCOUPLING_SRC = "http://files.salome-platform.org/Salome/other/medCoupling-9.3.0.tar.gz"
+    MEDCOUPLING_SRC = (
+        "http://files.salome-platform.org/Salome/other/medCoupling-9.3.0.tar.gz"
+    )
 
     # Environment variables
     CMAKE_EXE = os.environ.get("CMAKE_EXE", shutil.which("cmake"))
@@ -35,30 +36,32 @@ def download_build_medcoupling():
     if BUILD_TYPE is None:
         BUILD_TYPE = "Release"
 
-    version_medcoupling = MEDCOUPLING_SRC.partition("medCoupling-")[2].partition(".tar")[0]
+    version_medcoupling = MEDCOUPLING_SRC.partition("medCoupling-")[2].partition(
+        ".tar"
+    )[0]
     sourcedir = os.path.join(basedir, f"MEDCOUPLING-{version_medcoupling}")
     configdir = os.path.join(basedir, f"CONFIGURATION_{version_medcoupling}")
     installdir = os.path.join(basedir, "medcoupling")
 
-    def check_os():
-        system = platform.system()
-        if system == "Darwin":
-            print("macOS not yet supported")
-            sys.exit(1)
-
     def check_cmake():
         if not CMAKE_EXE:
-            print("cmake executable not found. Set CMAKE_EXE environment or update your path")
+            print(
+                "cmake executable not found. Set CMAKE_EXE environment or update your path"
+            )
             sys.exit(1)
 
     def check_swig():
         if not shutil.which("swig"):
-            print("swig executable not found. Set SWIG_ROOT_DIR environment or update your path")
+            print(
+                "swig executable not found. Set SWIG_ROOT_DIR environment or update your path"
+            )
             sys.exit(1)
 
     def check_python():
         if not shutil.which("python") and not shutil.which("python3"):
-            print("python or python3 executable not found. Set PYTHON_ROOT_DIR environment or update your path")
+            print(
+                "python or python3 executable not found. Set PYTHON_ROOT_DIR environment or update your path"
+            )
             sys.exit(1)
         else:
             try:
@@ -67,33 +70,7 @@ def download_build_medcoupling():
                 print("numpy is not installed")
                 sys.exit(1)
 
-    def patch_MEDCoupling_Swig(memarray):
-        with open(memarray, "r") as f:
-            lines = f.read()
-
-        lines = lines.replace("char *pt=PyUnicode_AsUTF8AndSize(obj, &sz)",
-                              "const char *pt=PyUnicode_AsUTF8AndSize(obj, &sz)")
-        with open(memarray, "w") as f:
-            f.write(lines)
-
-    def patch_MEDCoupling_Swig_Linux(cmakelists):
-        with open(cmakelists, "r") as f:
-            lines = f.read()
-
-        lines = lines.replace("${PYTHON_LIBRARIES} ", "")
-        with open(cmakelists, "w") as f:
-            f.write(lines)
-
-    def patch_MEDCoupling_Swig_Windows(cmakelists):
-        with open(cmakelists, "r") as f:
-            lines = f.readlines()
-
-        lines.insert(94, '  SET(SWIG_MODULE_MEDCouplingCompat_EXTRA_FLAGS "${NUMPY_DEFINITIONS};${SCIPY_DEFINITIONS}")\n')
-        with open(cmakelists, "w") as f:
-            f.writelines(lines)
-
     # Preliminary checks
-    check_os()
     check_cmake()
     check_swig()
     check_python()
@@ -112,46 +89,47 @@ def download_build_medcoupling():
 
     # Apply patches
     print("Applying patch...")
-    memarray = os.path.join(sourcedir, "src", "MEDCoupling_Swig", "MEDCouplingMemArray.i")
-    patch_MEDCoupling_Swig(memarray)
-
-    if platform.system() == "Linux":
-        print(f"Applying patch for Linux...")
-        cmakelists = os.path.join(sourcedir, "src", "MEDCoupling_Swig", "CMakeLists.txt")
-        patch_MEDCoupling_Swig_Linux(cmakelists)
-        cmakelists = os.path.join(sourcedir, "src", "PyWrapping", "CMakeLists.txt")
-        patch_MEDCoupling_Swig_Linux(cmakelists)
-
-    if platform.system() == "Windows":
-        print(f"Applying patch for Windows...")
-        cmakelists = os.path.join(sourcedir, "src", "MEDCoupling_Swig", "CMakeLists.txt")
-        patch_MEDCoupling_Swig_Windows(cmakelists)
+    subprocess.run(
+        "patch -s -p0 < patches/configuration.patch", cwd=basedir, shell=True
+    )
+    subprocess.run("patch -s -p0 < patches/medcoupling.patch", cwd=basedir, shell=True)
 
     # Building
     builddir = os.path.join(sourcedir, "build")
     os.makedirs(builddir, exist_ok=True)
 
-    cmake_args = [CMAKE_EXE, sourcedir,
-                  f"-DCONFIGURATION_ROOT_DIR={configdir}",
-                  f"-DCMAKE_INSTALL_PREFIX=install",
-                  "-DMEDCOUPLING_MICROMED=ON",
-                  "-DMEDCOUPLING_BUILD_DOC=OFF",
-                  "-DMEDCOUPLING_ENABLE_PARTITIONER=OFF",
-                  "-DMEDCOUPLING_BUILD_TESTS=OFF",
-                  "-DMEDCOUPLING_ENABLE_RENUMBER=OFF",
-                  "-DMEDCOUPLING_WITH_FILE_EXAMPLES=OFF"
-                  "-DCMAKE_BUILD_TYPE=" + BUILD_TYPE]
+    cmake_args = [
+        CMAKE_EXE,
+        sourcedir,
+        f"-DCONFIGURATION_ROOT_DIR={configdir}",
+        f"-DCMAKE_INSTALL_PREFIX=install",
+        "-DMEDCOUPLING_MICROMED=ON",
+        "-DMEDCOUPLING_BUILD_DOC=OFF",
+        "-DMEDCOUPLING_ENABLE_PARTITIONER=OFF",
+        "-DMEDCOUPLING_BUILD_TESTS=OFF",
+        "-DMEDCOUPLING_ENABLE_RENUMBER=OFF",
+        "-DMEDCOUPLING_WITH_FILE_EXAMPLES=OFF",
+        "-DCMAKE_BUILD_TYPE=" + BUILD_TYPE,
+    ]
     if PYTHON_ROOT_DIR is not None:
         cmake_args += [f"-DPYTHON_ROOT_DIR={PYTHON_ROOT_DIR}"]
     if SWIG_ROOT_DIR is not None:
         cmake_args += [f"-DSWIG_ROOT_DIR={SWIG_ROOT_DIR}"]
-    if platform.system() == "Linux":
+    if platform.system() != "Windows":
         cmake_args += ["-DMEDCOUPLING_BUILD_STATIC=ON"]
 
     env = os.environ.copy()
     subprocess.check_call(cmake_args, cwd=builddir, env=env)
 
-    cmake_build_args = [CMAKE_EXE, "--build", ".", "--config", BUILD_TYPE, "--target", "install"]
+    cmake_build_args = [
+        CMAKE_EXE,
+        "--build",
+        ".",
+        "--config",
+        BUILD_TYPE,
+        "--target",
+        "install",
+    ]
     subprocess.check_call(cmake_build_args, cwd=builddir, env=env)
 
     print("Copying C++/Python libary files...")
@@ -178,18 +156,17 @@ class build_py(_build_py):
         _build_py.run(self)
 
 
-def read(fname):
-    return codecs.open(os.path.join(basedir, fname), encoding="utf-8").read()
-
-
 # Force platform-specific bdist
 # https://stackoverflow.com/questions/45150304/how-to-force-a-python-wheel-to-be-platform-specific-when-building-it
 try:
     from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+
     class bdist_wheel(_bdist_wheel):
         def finalize_options(self):
             _bdist_wheel.finalize_options(self)
             self.root_is_pure = False
+
+
 except ImportError:
     bdist_wheel = None
 
@@ -204,15 +181,14 @@ setup(
     author_email=__email__,
     install_requires=["numpy"],
     description="Python repackaging of the MEDCoupling library",
-    long_description=read("README.md"),
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     license=__license__,
     classifiers=[
         __license__,
         __status__,
         # See <https://pypi.org/classifiers/> for all classifiers.
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: Unix",
+        "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
