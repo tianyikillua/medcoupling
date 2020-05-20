148b262ed48b216692aefa33a24dd1f65ac93340
LI Tianyi <tianyi.li@3ds.com>
2020-05-20 09:04:41 +0200

    Hot fix for Linux

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -14,6 +14,7 @@ cd ~/
 for PYVERSION in 7; do
     export PYTHON_ROOT_DIR=/opt/python/cp3${PYVERSION}-cp3${PYVERSION}m
     ${PYTHON_ROOT_DIR}/bin/pip install cmake numpy scipy
+    touch ${PYTHON_ROOT_DIR}/lib/libpython3.${PYVERSION}m.so
     export CMAKE_EXE=${PYTHON_ROOT_DIR}/bin/cmake
     ${PYTHON_ROOT_DIR}/bin/python3 setup.py bdist_wheel --plat-name manylinux2010_x86_64
 done
